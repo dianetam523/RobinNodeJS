99be6a377e3e08da5d46056a99bcb5efbbe48148
Diane Tam <diane.tam523@gmail.com>
2016-10-08 11:43:32 -0400

    Index page routes correctly, need to pass data from json objects over now

--- quote.js
+++ quote.js
@@ -11,9 +11,9 @@ app.get('/', function(req, res) {
    res.send('Hello World');
 });
 
-// app.get('/index.htm', function(req, res) {
-//    res.sendFile( __dirname + "/" + "index.htm" );
-// });
+app.get('/index.htm', function(req, res) {
+   res.sendFile( __dirname + "/" + "index.htm" );
+});
 
 var server = app.listen(8081, function () {
    var host = server.address().address
