6614781ee6c33744fc7cbc0d3338a37b162854ae
Gahyun (Susie) Kim <gahyunkim@umass.edu>
2016-10-09 00:07:00 -0400

    Fixed clock

--- public/js/clock.js
+++ public/js/clock.js
@@ -2,6 +2,7 @@ function startTime() {
     var today = new Date();
     var h = today.getHours();
     if (h>12) {h= h-12};
+    if (h==0) {h=12};
     var m = today.getMinutes();
     var s = today.getSeconds();
     m = checkTime(m);
