8a404c1b37929c36253690f7b8c35336d9a50a5a
Anthony Chan <anthony.me.chan@gmail.com>
2016-10-08 21:30:24 -0400

    more js insertions

--- public/js/index.js
+++ public/js/index.js
@@ -1,17 +1,31 @@
 $(document).ready(function(){
+    /*
+    JavaScript, JQuery, Express function to retrieve data from Node.JS server
+    */
     $("#attendance").click(function(){
+        
         var TEXT = $.get('/presence').then(function(response){
+            //clear div with id='result'
+            $("#result").html(""); 
             var i;
-            $("#result").html("");
             for( i = 0; i < response.length; i++){
+                //iterate through each user, append information to div
                 var person = response[i];
+                //data check
                 console.log(response[i]);
                 console.log(person.user.name);
-            $("#result").append("<div class='text-left'>"+person.user.name+" ...... "+ person.user.primary_email.email+"</div>");     
+            $("#result").append("<div class='text-left'>"+person.user.name+" ...... "+ person.user.primary_email.email+"</div>");
+                
             }
             
         });
        
+    });
+    
+    $('#roomBtn').click(function(){
+        var roomNum = $('#roomBtn').val;
+        //deliver roomNum value to quotes.js GET function for presence.
+        
     });
 
 });
--- quote.js
+++ quote.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var Robin = require('robin-js-sdk');
-
+var roomNum;
 var robin = new Robin('OukU3HqR6O3yPPfEpQbN76lsqhCAZ9B0A7DgbyV9hWpU98evUyy8LPFvfHXr0suMdZbwp0neSPQPE6DK0K03ZN2D0ifvWJuR6oAIeBlIjC6HgUK4B1Mt3AUtkS9AJRko');
 var app = express();
 
@@ -24,16 +24,24 @@ var server = app.listen(8081, function () {
 
 app.get('/presence', function(req, res) {
 var peoplePresent = [];
-    //TODO catch error when no one in vicinity
+    //TODO catch error when no one in vicinity 16008
+    
 robin.api.spaces.presence.get(16008).then(function (response) {
   peoplePresent = response.getData();
   res.json(peoplePresent);
     if(peoplePresent[0] != null){
         console.log(peoplePresent[0].space_id);    
     }
+    
   
   });
+   
 });
+
+
+
+
+
 
 
 
